1bcf27768af87f300195bb5dd84cb7bc1d1b27d8
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-01-19 15:41:14 -0700

    added simple timer to order
    added simple timer to order pog

--- components/Order.jsx
+++ components/Order.jsx
@@ -7,12 +7,33 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
 export default function Order(){
   let width = window.innerWidth-16
   let height = window.innerHeight*2/3
+  let counter = 10
 
 	const setup = (p5, canvasParentRef) => {
 		let cnv = p5.createCanvas(width, height).parent(canvasParentRef);
       cnv.mousePressed(() => {
 
       })
+      
+
+  
+  var timer = p5.createDiv('');
+  //timer.html(counter); //insert text
+  timer.style('font-size', '1rem');
+  timer.style('color', 'white')
+  timer.position(width/2-123, height/3+24);
+  timer.style('font-family', 'monospace')
+
+  
+  function timeIt(){
+    counter = counter - 0.5
+    timer.html(counter)
+  }
+  
+  setInterval(timeIt, 1000);//native function 1000ms =1 s
+  //request animationframe is also another func
+
+
 	};
 
   const draw = (p5) => {
