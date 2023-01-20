6e1dd9b4e2466ff333ef67eb933997ddc4150a03
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-01-19 21:16:45 -0700

    added a better timer for order
    added a better timer for order pogu

--- components/Order.jsx
+++ components/Order.jsx
@@ -7,7 +7,8 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
 export default function Order(){
   let width = window.innerWidth-16
   let height = window.innerHeight*2/3
-  let counter = 10
+  let counter = 3
+  let round = 1
 
 	const setup = (p5, canvasParentRef) => {
 		let cnv = p5.createCanvas(width, height).parent(canvasParentRef);
@@ -17,8 +18,7 @@ export default function Order(){
       
 
   
-  var timer = p5.createDiv('');
-  //timer.html(counter); //insert text
+  var timer = p5.createDiv(counter);
   timer.style('font-size', '1rem');
   timer.style('color', 'white')
   timer.position(width/2-123, height/3+24);
@@ -26,8 +26,15 @@ export default function Order(){
 
   
   function timeIt(){
-    counter = counter - 0.5
-    timer.html(counter)
+    if(counter >= 0){
+      counter = counter - 0.5
+      console.log(counter)
+      timer.html(counter)
+    }else{
+      counter = 0
+      round ++
+      counter = round + 3
+    }
   }
   
   setInterval(timeIt, 1000);//native function 1000ms =1 s
