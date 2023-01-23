58c61dc99ffe46adff57063b03dfb393068690b6
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-01-23 12:04:26 -0700

    added start button and title to order
    added start button and title to order pogman

--- components/Order.jsx
+++ components/Order.jsx
@@ -8,21 +8,76 @@ export default function Order(){
   let width = window.innerWidth-16
   let height = window.innerHeight*2/3
   let counter = 3
-  let round = 1
+  let round = 0
+  let lower = round
+  let higher = round + 1
+  let number
+
+  let titleText
+  let descriptionText
+  let instructionsText
+
+  let startButton
+
 
 	const setup = (p5, canvasParentRef) => {
-		let cnv = p5.createCanvas(width, height).parent(canvasParentRef);
+		let cnv = p5.createCanvas(width, height).parent(canvasParentRef)
       cnv.mousePressed(() => {
 
       })
+
+      function randomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+      }
+      
+    titleText = p5.createDiv('Test of Tenacity')
+    titleText.style('font-size', '3rem');
+    titleText.style('color', 'white')
+    titleText.position(width/2-211, height/5);
+    titleText.style('font-family', 'monospace')
+
+    descriptionText = p5.createDiv('when this box turns green, click as swiftly as you can')
+    descriptionText.style('font-size', '1rem');
+    descriptionText.style('color', 'white')
+    descriptionText.position(width/2-237.5, height/3);
+    descriptionText.style('font-family', 'monospace')
+
+    instructionsText = p5.createDiv('click the green box to begin')
+    instructionsText.style('font-size', '1rem');
+    instructionsText.style('color', 'white')
+    instructionsText.position(width/2-123, height/3+24);
+    instructionsText.style('font-family', 'monospace')
+
+    startButton = p5.createButton('start')
+    startButton.position(width/2-69, height/3+120)
+    startButton.style('font-size', '3rem');
+    startButton.style('color', 'black')
+    startButton.style('background', "darkgrey")
+    startButton.style('border-width', '3px')
+    startButton.style('font-family', 'monospace')
+    startButton.mousePressed(showNumber)
+
       
 
   
-    var timerText = p5.createDiv(counter);
-    timerText.style('font-size', '2rem');
+    let timerText = p5.createDiv(`${counter} sec`)
+    timerText.style('font-size', '2rem')
     timerText.style('color', 'white')
-    timerText.position(width/2-123, height/3+24);
+    timerText.position(width/2-44, height/3+120)
     timerText.style('font-family', 'monospace')
+    timerText.hide()
+
+    function showNumber() {
+      timerText.show()
+      titleText.hide()
+      descriptionText.hide()
+      instructionsText.hide()
+      startButton.hide()
+
+      //number = randomInteger()
+    }
+
+
 
     
     function timer(){
@@ -31,14 +86,21 @@ export default function Order(){
         //console.log(counter)
         timerText.html(`${counter} sec`)
       }else{
+        lower = round
+        higher = round + 1
+
         counter = 0
         round ++
-        counter = round + 3
+        counter = round + 4
+        console.log(10 ** lower, 10 ** higher)
+        console.log(lower, higher)
       }
     }
     
     setInterval(timer, 1000);//native function 1000ms =1 s
     //request animationframe is also another func
+
+
 
 
 	};
