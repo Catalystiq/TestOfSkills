c4bbea8ea6aa66f2dcd4a57852d1de3d1631e773
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-01-23 17:42:20 -0700

    added guessing screen
    added guessing screen so it can be cool pog

--- components/Order.jsx
+++ components/Order.jsx
@@ -8,16 +8,24 @@ export default function Order(){
   let width = window.innerWidth-16
   let height = window.innerHeight*2/3
   let counter = 3
-  let round = 0
+  let round = -1
   let lower = round
   let higher = round + 1
   let number
+  let guess
 
   let titleText
   let descriptionText
   let instructionsText
+  let numberText
+  let timerText
 
   let startButton
+
+  let guessTitle
+  let guessInstructions
+  let guessInput
+  let guessButton
 
 
 	const setup = (p5, canvasParentRef) => {
@@ -57,53 +65,119 @@ export default function Order(){
     startButton.style('font-family', 'monospace')
     startButton.mousePressed(showNumber)
 
-      
+    timerText = p5.createDiv(``)
+    numberText = p5.createDiv(``)
+    guessTitle = p5.createDiv(``)
+    guessInstructions = p5.createDiv(``)
+    guessInput = p5.createInput(``)
+    guessButton = p5.createButton(``)
 
   
-    let timerText = p5.createDiv(`${counter} sec`)
-    timerText.style('font-size', '2rem')
-    timerText.style('color', 'white')
-    timerText.position(width/2-44, height/3+120)
-    timerText.style('font-family', 'monospace')
-    timerText.hide()
+
 
     function showNumber() {
-      timerText.show()
+      numberText.html(``)
+      numberText.style('font-size', '4rem')
+      numberText.style('color', 'white')
+      numberText.position(width/2-44, height/3)
+      numberText.style('font-family', 'monospace')
+
       titleText.hide()
       descriptionText.hide()
       instructionsText.hide()
       startButton.hide()
 
-      //number = randomInteger()
+      counter = 0
+      round ++
+      lower = round
+      counter = round + 4
+      higher = round + 1
+      number = randomInteger(10 ** lower, 10 ** higher)
+      numberText.html(number)
+
+      timerText.html(`${counter} sec`)
+      timerText.style('font-size', '2rem')
+      timerText.style('color', 'white')
+      timerText.position(width/2-44, height/3+120)
+      timerText.style('font-family', 'monospace')
+  
+
+
+      
+
+      function timer(){
+        if(counter > 0){
+          counter = counter - 1
+          //console.log(counter)
+          timerText.html(`${counter} sec`)
+        }else{
+          // lower = round
+          // higher = round + 1
+  
+          // counter = 0
+          // round ++
+          // counter = round + 4
+          // console.log(10 ** lower, 10 ** higher)
+          // console.log(lower, higher)
+          showGuess()
+        }
+      }
+
+
+      
+      setInterval(timer, 1000)
     }
 
+    function showGuess() {
+      timerText.hide()
+      numberText.hide()
 
+      guessTitle.html('Enter the Number here...')
+      guessTitle.style('font-size', '2rem');
+      guessTitle.style('color', 'white')
+      guessTitle.position(width/2-185, height/3);
+      guessTitle.style('font-family', 'monospace')
 
-    
-    function timer(){
-      if(counter >= 0){
-        counter = counter - 0.5
-        //console.log(counter)
-        timerText.html(`${counter} sec`)
-      }else{
-        lower = round
-        higher = round + 1
+      guessInstructions.html(`Press enter or the submit button to enter guess`)
+      guessInstructions.style('font-size', '1rem');
+      guessInstructions.style('color', 'white')
+      guessInstructions.position(width/2-44, height/2);
+      guessInstructions.style('font-family', 'monospace')
 
-        counter = 0
-        round ++
-        counter = round + 4
-        console.log(10 ** lower, 10 ** higher)
-        console.log(lower, higher)
+      guessInput.position(width/2-69, height-200)
+      guessInput.style('font-size', '1rem');
+      guessInput.style('font-family', 'monospace')
+      guessInput.input(guessInputListener)
+
+      guessButton.html('submit')
+      guessButton.position(width/2-69, height-120)
+      guessButton.style('font-size', '3rem');
+      guessButton.style('color', 'black')
+      guessButton.style('background', "darkgrey")
+      guessButton.style('border-width', '3px')
+      guessButton.style('font-family', 'monospace')
+
+      guessButton.mousePressed(checkGuess)
+
+      function guessInputListener() {
+        guess = this.value()
       }
     }
+
+    function checkGuess() {
+      if(guess == number){
+        console.log('mega balls')
+      }
+    }
+
     
-    setInterval(timer, 1000);//native function 1000ms =1 s
-    //request animationframe is also another func
 
 
 
 
-	};
+
+
+	}
 
   const draw = (p5) => {
 		p5.background(0)
